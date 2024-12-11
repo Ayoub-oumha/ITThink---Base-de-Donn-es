033b2f340f4ced702adf849e840e06bbf1fe5f18
ayoub oumha <ayyouboumha@gmail.com>
2024-12-11 21:27:19 +0100

    add file of sql and diagrames

---
+++ itthink.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 11 déc. 2024 à 21:25
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `itthink`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_categorie` int(11) NOT NULL,
+  `nom_categorie` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id_categorie`, `nom_categorie`) VALUES
+(1, 'design'),
+(2, 'design'),
+(3, 'design'),
+(4, 'design'),
+(5, 'design');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `freelances`
+--
+
+CREATE TABLE `freelances` (
+  `id_freelance` int(11) NOT NULL,
+  `nom_freelace` varchar(50) DEFAULT NULL,
+  `competence` varchar(500) DEFAULT NULL,
+  `id_utilisateur` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `freelances`
+--
+
+INSERT INTO `freelances` (`id_freelance`, `nom_freelace`, `competence`, `id_utilisateur`) VALUES
+(1, 'Mohmed', 'figma , xs ', NULL),
+(2, 'Mohmed', 'figma , xs ', NULL),
+(3, 'Mohmed', 'figma , xs ', NULL),
+(4, 'Mohmed', 'figma , xs ', NULL),
+(5, 'Mohmed', 'figma , xs ', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres`
+--
+
+CREATE TABLE `offres` (
+  `id_offre` int(11) NOT NULL,
+  `montant` decimal(10,0) DEFAULT NULL,
+  `delai` date DEFAULT NULL,
+  `id_freelance` int(11) DEFAULT NULL,
+  `id_project` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `offres`
+--
+
+INSERT INTO `offres` (`id_offre`, `montant`, `delai`, `id_freelance`, `id_project`) VALUES
+(1, 125, '0000-00-00', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id_project` int(11) NOT NULL,
+  `titre_project` varchar(50) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `id_sous_categorie` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sous_categorie`
+--
+
+CREATE TABLE `sous_categorie` (
+  `id_sous_categorie` int(11) NOT NULL,
+  `nom_sous_categorie` varchar(50) DEFAULT NULL,
+  `id_categorie` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `sous_categorie`
+--
+
+INSERT INTO `sous_categorie` (`id_sous_categorie`, `nom_sous_categorie`, `id_categorie`) VALUES
+(1, 'web design', NULL),
+(2, 'web design', NULL),
+(3, 'web design', NULL),
+(4, 'web design', NULL),
+(5, 'web design', NULL),
+(6, 'web design', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `temoignage`
+--
+
+CREATE TABLE `temoignage` (
+  `id_temoignage` int(11) NOT NULL,
+  `commentaire` varchar(500) DEFAULT NULL,
+  `id_utilisateur` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `temoignage`
+--
+
+INSERT INTO `temoignage` (`id_temoignage`, `commentaire`, `id_utilisateur`) VALUES
+(1, 'this is commentaire in temoignage', NULL),
+(2, 'this is commentaire in temoignage', NULL),
+(3, 'this is commentaire in temoignage', NULL),
+(4, 'this is commentaire in temoignage', NULL),
+(5, 'this is commentaire in temoignage', NULL),
+(6, 'this is commentaire in temoignage', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id_utilisateur` int(11) NOT NULL,
+  `nom_utilisateur` varchar(50) NOT NULL,
+  `mot_de_passe` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_utilisateur`, `nom_utilisateur`, `mot_de_passe`, `email`) VALUES
+(1, 'Ayoub', 'motdepasse', 'ayoubgmail'),
+(2, 'Younes', '002jfl', 'younes@2024');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Index pour la table `freelances`
+--
+ALTER TABLE `freelances`
+  ADD PRIMARY KEY (`id_freelance`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD PRIMARY KEY (`id_offre`),
+  ADD KEY `id_freelance` (`id_freelance`),
+  ADD KEY `id_project` (`id_project`);
+
+--
+-- Index pour la table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id_project`),
+  ADD KEY `id_categorie` (`id_categorie`),
+  ADD KEY `id_sous_categorie` (`id_sous_categorie`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  ADD PRIMARY KEY (`id_sous_categorie`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Index pour la table `temoignage`
+--
+ALTER TABLE `temoignage`
+  ADD PRIMARY KEY (`id_temoignage`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id_utilisateur`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `freelances`
+--
+ALTER TABLE `freelances`
+  MODIFY `id_freelance` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `offres`
+--
+ALTER TABLE `offres`
+  MODIFY `id_offre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  MODIFY `id_sous_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `temoignage`
+--
+ALTER TABLE `temoignage`
+  MODIFY `id_temoignage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `freelances`
+--
+ALTER TABLE `freelances`
+  ADD CONSTRAINT `freelances_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD CONSTRAINT `offres_ibfk_1` FOREIGN KEY (`id_freelance`) REFERENCES `freelances` (`id_freelance`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `offres_ibfk_2` FOREIGN KEY (`id_project`) REFERENCES `projects` (`id_project`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`id_sous_categorie`) REFERENCES `sous_categorie` (`id_sous_categorie`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `projects_ibfk_3` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `sous_categorie`
+--
+ALTER TABLE `sous_categorie`
+  ADD CONSTRAINT `sous_categorie_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `temoignage`
+--
+ALTER TABLE `temoignage`
+  ADD CONSTRAINT `temoignage_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
